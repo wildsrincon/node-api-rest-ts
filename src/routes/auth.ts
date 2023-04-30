1873ed4e25588a93c5c21a3912120ccd40c1112d
Wilder Rincon <wildsrincon.developer@gmail.com>
2023-04-29 23:15:38 -0500

    feat: adding middleware log.ts

---
+++ src/routes/auth.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { registerCtrl, loginCtrl } from "../controllers/auth";
+
+const router = Router();
+router.post("/register", registerCtrl);
+router.post("/login", loginCtrl);
+
+export { router };
